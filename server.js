88f98a07c2907d341e034be010a4ba7de31ecb4b
satyam720 <psatyam1618@gmail.com>
2026-04-28 05:38:17 +0530

    add starter

---
+++ server.js
@@ -0,0 +1,11 @@
+import { configDotenv } from 'dotenv';
+
+configDotenv({ path: './config.env', debug: true, encodeing: 'UTF-8'});
+
+const { default: app } await import('./app.js');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`istening on port ${port}`);
+})
