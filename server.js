e3b5f689abc3c31daf929a93f1dd9abfde139e2f
satyam720 <psatyam1618@gmail.com>
2026-05-09 06:10:03 +0530

    fix(Refactor): refactor-into-mvc

--- server.js
+++ server.js
@@ -8,37 +8,6 @@ const { default: app } = await import('./app.js');
 const DB = process.env.DATABASE.replace('<db_password>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB).then(() => console.log('DB connection succesfull'))
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "A tour must have a name"],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have price'],
-    }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-    name: 'The Park chapter',
-    price: 997
-
-});
-
-testTour.save()
-.then(doc => {
-    console.log(doc);
-})
-.catch(err => {
-    console.log("Error::", err);
-});
 
 const port = process.env.PORT || 3000;
 
