96ca4fa352207ce949c4d508999c91598a0e9940
satyam720 <psatyam1618@gmail.com>
2026-07-08 05:09:52 +0530

    updatePassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,18 @@ const signToken = (id) => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    },
+  });
+};
+
 const signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -22,15 +34,7 @@ const signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
   });
 
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 const login = catchAsync(async (req, res, next) => {
@@ -48,11 +52,7 @@ const login = catchAsync(async (req, res, next) => {
   }
 
   // 3) if everything is okay, send the token to client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 const protect = catchAsync(async (req, res, next) => {
@@ -171,11 +171,26 @@ const resetPassword = catchAsync(async (req, res, next) => {
   // 3) udpate changedPasswordAt property for the user
 
   // 4) Log the user in, send JWT
-  const token = signToken(user._id);
-  res.status(201).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 201, res);
 });
 
-export { signup, login, protect, restrictTo, forgotPassword, resetPassword };
+const updatePassword = catchAsync(async (req, res, next) => {
+  //1) get the user from collection
+  const user = await User.findById(req.user.id).select('+password');
+  console.log(user);
+  //2) check if posted password is correct
+  if (!await user.correctPassword(req.body.passwordCurrent, user.password)) {
+    return next(
+      new AppError('Invalid current password! please try again', 401),
+    );
+  }
+  //3) if the password is correct then updated the password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  //4) log user in, send JWT
+  createSendToken(user, 200, res);
+});
+
+export { signup, login, protect, restrictTo, forgotPassword, resetPassword, updatePassword };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,14 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/userController.js';
-import { forgotPassword, login, resetPassword, signup } from '../controllers/authController.js';
+import {
+  forgotPassword,
+  login,
+  protect,
+  resetPassword,
+  signup,
+  updatePassword,
+} from '../controllers/authController.js';
 
 const router = express.Router();
 router.post('/signup', signup);
@@ -14,6 +21,7 @@ router.post('/login', login);
 
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
+router.patch('/updatePassword', protect, updatePassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
